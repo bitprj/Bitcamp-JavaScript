4ec4b79873a8c82c6997a7d29258b1655c1c5fe0
Kyle Begovich <begovichkyle@gmail.com>
2020-10-06 21:17:05 -0500

    adding a hw for week 1

---
+++ Week1/Homework.js
@@ -0,0 +1,25 @@
+// FizzBuzz In SPACE
+const argumentValue = process.argv[2];
+const numericValue = Number(argumentValue);
+
+// For every number 1 ... numericValue:
+var i;
+for (i = 1; i <= numericValue; i ++) {
+
+  // if number is divisible by 3 but not 5, print 👽
+  if (i % 3 == 0 && i % 5 != 0) {
+    console.log("👽 " + i);
+
+  // if number is divisible by 5 but not 3, print 🚀
+  } else if (i % 5 == 0 && i % 3 != 0) {
+    console.log("🚀 " + i);
+
+  // if number is divisible by both 3 and 5, print 🛰
+  } else if (i % 3 == 0 && i % 5 == 0) {
+    console.log("🛰 " + i);
+
+  // if number is not divisible by either 3 or 5, print 💩
+  } else {
+    console.log("💩 " + i);
+  }
+}
